40e436d59c8f6f4113590061eccb0ad756cddf5d
Matt Seath <seath.dev@gmail.com>
2022-12-27 15:30:12 +1000

    dynamic uri functionality

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,33 @@
 #[macro_use]
 extern crate rocket;
 
-/// Handles GET requests to the root path ("/").
-///
-/// Returns a static string with the message "Hello, World!".
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct User {
+    uuid: String,
+    name: String,
+    email: String,
+    active: bool,
+}
+
+lazy_static! {
+    static ref USERS: HashMap<&'static str, User> = {
+        let mut map = HashMap::new();
+        map.insert(
+            "3e3dd4ae-3c37-40c6-aa64-7061f284ce28",
+            User {
+                uuid: String::from("3e3dd4ae-3c37-40c6-aa64-7061f284ce28"),
+                name: String::from("John Doe"),
+                email: String::from("johndoe@gmail.com"),
+                active: true,
+            },
+        );
+        map
+    };
+}
+
 #[get("/")]
 async fn index() -> &'static str {
     "Hello, World!"
@@ -14,11 +38,20 @@ async fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[get("/user/<uuid>", rank = 1, format = "text/html")]
+async fn user(uuid: &str) -> String {
+    let user = USERS.get(uuid);
+    match user {
+        Some(u) => format!("Found user: {:?}", u),
+        None => String::from("User not found"),
+    }
+}
+
 /// Launches the Rocket web server with the provided routes.
 #[rocket::main]
 async fn main() {
     rocket::build()
-        .mount("/", routes![index, greet])
+        .mount("/", routes![index, greet, user])
         .launch()
         .await
         .map_err(|err| println!("{:?}", err))
