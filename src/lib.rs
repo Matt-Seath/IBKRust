2d9de2144c1a073539ae550293ee326b99ef7927
Matt Seath <seath.dev@gmail.com>
2022-12-27 23:13:41 +1000

    env variables for rocket config

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+extern crate dotenv;
 
 use crate::routes::home;
 use rocket::{Build, Rocket};
@@ -7,6 +8,10 @@ use rocket::{Build, Rocket};
 pub mod routes;
 
 pub async fn setup_rocket() -> Rocket<Build> {
+    dotenv::dotenv().ok();
+    for (k, v) in std::env::vars() {
+        eprintln!("{}={}", k, v);
+    }
     let my_rocket = rocket::build().mount("/", routes![home]);
     my_rocket
 }
