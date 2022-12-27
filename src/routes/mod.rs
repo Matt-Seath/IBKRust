36183e7cd8d423934954a734d83bb15cdac85a05
Matt Seath <seath.dev@gmail.com>
2022-12-28 00:26:31 +1000

    served static html

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,15 @@
+use std::env;
+use std::io;
+use std::path::Path;
+
+use rocket::fs::NamedFile;
+
 #[get("/")]
-pub async fn home() -> &'static str {
-    "Success!!!!"
+pub async fn home() -> io::Result<NamedFile> {
+    let page_directory_path = get_directory_path();
+    NamedFile::open(Path::new(&page_directory_path).join("index.html")).await
+}
+
+fn get_directory_path() -> String {
+    format!("{}/frontend/templates/", env!("CARGO_MANIFEST_DIR"))
 }
