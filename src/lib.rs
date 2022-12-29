fdf3f9511f5b006197233ccf4c904a7b4284fb70
Matt Seath <seath.dev@gmail.com>
2022-12-29 21:55:32 +1000

    added mysql connection

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,23 @@ extern crate rocket;
 extern crate dotenv;
 
 use crate::routes::{files, home};
+use mysql::prelude::*;
+use mysql::*;
 use rocket::{Build, Rocket};
+use std::env;
 
 pub mod routes;
 
 pub async fn setup_rocket() -> Rocket<Build> {
     dotenv::dotenv().ok();
-    for (k, v) in std::env::vars() {
+    let mysql_url: String = env::var("MYSQL_URL")
+        .expect("mysql_url variable should be set in an '.env' file in project root directory");
+        let pool = Pool::new(url)?;
+
+        let mut conn = pool.get_conn()?;
+        
+    
+    for (k, v) in env::vars() {
         eprintln!("{}={}", k, v);
     }
     let my_rocket = rocket::build().mount("/", routes![home, files]);
